fd7138e642df5e5fe41a0ac7766120ca1903ca4d
Tosasitill <66861785+tosasitill@users.noreply.github.com>
2024-02-25 22:25:19 +0800

    Update port.sh

--- port.sh
+++ port.sh
@@ -459,11 +459,11 @@ green "添加动画多线程支持"
 echo "persist.sys.miui_animator_sched.sched_threads=2" >> build/portrom/images/product/etc/build.prop
 
 green "添加 Via 浏览器作为默认浏览器存在"
-mkdir build/portrom/images/product/app/Via
-cp -rf devices/via.apk build/portrom/images/product/app/Via/
-green "添加 Magisk 26.4"
-mkdir build/portrom/images/product/app/Magisk
-cp -rf devices/Magisk-v26.4.apk build/portrom/images/product/app/Magisk/
+mkdir build/portrom/images/product/data-app/Via
+cp -rf devices/via.apk build/portrom/images/product/data-app/Via/
+green "添加 Magisk 26.4 APP"
+mkdir build/portrom/images/product/data-app/Magisk
+cp -rf devices/Magisk-v26.4.apk build/portrom/images/product/data-app/Magisk/
 baseDevicesAndroidOverlay=$(find build/baserom/images/product -type f -name "DevicesAndroidOverlay.apk")
 portDevicesAndroidOverlay=$(find build/portrom/images/product -type f -name "DevicesAndroidOverlay.apk")
 if [ -f "${baseDevicesAndroidOverlay}" ] && [ -f "${portDevicesAndroidOverlay}" ];then
